100dd96f3614039e9d60ecb31d6c13f84c7e76dd
João Henrique Santos Calasans Oliveira <jocs4@hotmail.com>
2020-03-08 19:30:53 -0300

    primeiro commit

---
+++ PossibleService/PossibleService/Contracts/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace PossibleService.Contracts
+{
+    [DataContract]
+    public class User
+    {
+        String _name;
+        String _password;
+
+        [DataMember]
+        public String Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        [DataMember]
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+    }
+}
